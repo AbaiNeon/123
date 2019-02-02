6c6c32bf4c582d42dfc960ed1d87f42cb8a7afa0
ТогузбаевА <abai2010@bk.ru>
2019-02-02 10:10:00 +0600

    Добавьте файлы проекта.

---
+++ ASP_Core_Exam_Auth_Cookie/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ASP_Core_Exam_Auth_Cookie.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ ASP_Core_Exam_Auth_Cookie/Models/User.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASP_Core_Exam_Auth_Cookie.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
